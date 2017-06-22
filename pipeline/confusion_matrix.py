3b068055d87b1165f947411c4aa90e774ad6e327
Marten Lienen <marten.lienen@gmail.com>
2017-06-22 22:07:39 +0200

    Compute the confusion matrix of a classifier

---
+++ pipeline/confusion_matrix.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+
+import argparse
+
+import h5py as h5
+import matplotlib.pyplot as pp
+import numpy as np
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("dataset")
+    args = parser.parse_args()
+
+    dataset_path = args.dataset
+
+    with h5.File(dataset_path, "r") as f:
+        label_index = list(f["label_index"])
+        logits = []
+        labels = []
+
+        def collect_logits(name, obj):
+            if isinstance(obj, h5.Group) and "data" in obj and "labels" in obj:
+                logits.append(np.array(obj["data"]))
+                labels.append(np.array(obj["labels"]))
+
+        f.visititems(collect_logits)
+
+    logits = np.concatenate(logits, axis=0)
+    labels = np.concatenate(labels, axis=0)
+
+    labels[labels == -1] = len(label_index)
+
+    predictions = np.argmax(logits, axis=-1)
+
+    label_index.append("<blank>")
+
+    cm = np.zeros((len(label_index), len(label_index)))
+    for i in range(len(label_index)):
+        fltr = labels == i
+        l = labels[fltr]
+        p = predictions[fltr]
+
+        for j in range(len(label_index)):
+            cm[i, j] = np.count_nonzero(p == j) / len(l)
+
+    ax = pp.gca()
+    tm = np.arange(len(label_index))
+    ax.xaxis.set_label_position("top")
+    ax.set_xticks(tm)
+    ax.set_xticklabels(label_index, rotation=90)
+    ax.set_yticks(tm)
+    ax.set_yticklabels(label_index)
+
+    ax.imshow(cm)
+    pp.show()
+
+
+if __name__ == "__main__":
+    main()
